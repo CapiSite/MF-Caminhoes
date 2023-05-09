2a13b2058b2baf294a9682503cee274b1f4b02a3
Rodrigo Antunes <rodrigo.teth@gmail.com>
2023-05-09 15:03:15 -0300

    feat: admin folder

--- my-app/src/Components/CardAdm.tsx
+++ my-app/src/Components/CardAdm.tsx
@@ -5,7 +5,7 @@ import { useRouter } from "next/router";
 export default function CardAdm(){
     const router = useRouter()
     return(
-        <div onClick={() => router.push(`/aprovarcard`)} className={style.card}>
+        <div onClick={() => router.push(`/admin/1`)} className={style.card}>
             <Image src={"/caminhao.jpeg"} alt="Caminhão" width={198} height={198} />
             <div>
             <h1>Caminhão</h1>
