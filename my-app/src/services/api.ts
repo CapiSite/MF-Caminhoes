a1e2d055870ec14a2ede2ada912ddab816991c07
Rodrigo Antunes <rodrigo.teth@gmail.com>
2023-05-18 13:09:25 -0300

    feat: docker conatiners

--- my-app/src/services/api.ts
+++ my-app/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: (process.env.REACT_BACK !== undefined? process.env.REACT_BACK :"http://154.49.246.233:5000")
+  baseURL: "http://154.49.246.233:5000"
 });
 
 export default instance;
