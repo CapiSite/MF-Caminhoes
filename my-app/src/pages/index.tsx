b043bfb51ab629f633d6a2f0f9d447150e31b140
hardkill551 <hardkill194@gmail.com>
2023-05-20 08:31:44 -0300

    feat/pop-up front

--- my-app/src/pages/index.tsx
+++ my-app/src/pages/index.tsx
@@ -9,10 +9,11 @@ import Carousel from "@/Components/Carousel";
 import { useCallback, useEffect, useState } from "react";
 import { getAllCarts } from "@/services/cart.services";
 import { AxiosError } from "axios";
+import { AiOutlineClose } from "react-icons/ai";
 
 export default function Home() {
   const [carrosel, setCarrosel] = useState([])
-
+  const [model, setModel] = useState<boolean>(true)
   const handleCall = useCallback(async () => {
     try {
       const cartsReceived = await getAllCarts()
@@ -37,7 +38,7 @@ export default function Home() {
       <div className={style.sidebar}>
         <Sidebar />
       </div>
-      <div className={style.model}>
+      {model && <div className={style.model}>
         <div className={style.modelLeft}>
         <h1>Tenha acesso a promoções, notícias e ofertas especiais cadastrando seu email!</h1>
         <input placeholder="Nome"></input>
@@ -45,9 +46,14 @@ export default function Home() {
         <button>Quero receber promoções, notícias e muito mais!</button>
         </div>
         <div className={style.modelRight}>
-          <h1>alçskfjdlksaçjfklsajflkjdslkajflks</h1>
+          <h1>Alugue o veículo ou máquina que você precisa em apenas alguns cliques!</h1>
+          <p>Cadastre seu email para ter acesso aos melhores descontos, promoções, ofertas especiais e muito mais!</p>
+            <p>Com seu email cadastrado, entraremos em contato com você levando as melhores noticias, descontos, promoções e ofertas do seu interesse!
+          </p>
+          <AiOutlineClose onClick={()=> setModel(false)}/>
         </div>
-      </div>
+      </div>}
+
       <main className={style.main}>
         <div className={style.center}>
           <Image className={style.banner} src={Banner} alt="Banner" />
